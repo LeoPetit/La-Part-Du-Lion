c76df21a504c37a441c5f5512cad3556260b64a7
schliermelvin <miliciiop.77>
2017-11-27 09:13:29 +0100

    navbar v1

--- Web/application/views/index.php
+++ Web/application/views/index.php
@@ -15,6 +15,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
     <meta charset="utf-8">
     <title>Test de la map</title>
+    <?php include("/Users/schliermelvin/Sites/projet_licence/La-Part-Du-Lion/Web/application/views/Nav/header.php") ?>
+    <?php include("/Users/schliermelvin/Sites/projet_licence/La-Part-Du-Lion/Web/application/views/Nav/navbar.php") ?>
+
+
 </head>
 
 
