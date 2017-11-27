a6ea79ceb564c0a1874b6bc4022ec7c42f4f8c8a
schliermelvin <miliciiop.77>
2017-11-27 09:03:54 +0100

    Navbar v1

---
+++ Web/application/views/Nav/navbar.php
@@ -0,0 +1,61 @@
+<nav class="navbar navbar-expand-lg navbar-fixed-top">
+    <a class="navbar-brand" href="#">La Part du Lion</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent" >
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="application/views/Accueil/index.php">Accueil <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="application/views/Forum/index.php">Forum</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Présentation</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link"  href="#"></a>
+            </li>
+        </ul>
+
+        <a class="brand" href="#"><img class="img-responsive logo" src="../divers/images/logos/png/logo.png" alt=""></a>
+
+
+        <ul class="navbar-nav">
+            <li class="nav-item dropdown " >
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Jeu
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Map</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Boutique</a>
+                </div>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Mon Compte</a>
+            </li>
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Mon Clan
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Progression</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Arbre de compétences</a>
+                </div>
+            </li>
+        </ul>
+
+<!--        <form class="form-inline ">-->
+<!--            <button class="btn btn-outline-success" type="button">Se Connecter</button>-->
+<!--        </form>-->
+    </div>
+</nav>
