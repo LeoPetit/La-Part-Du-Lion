5e16622f0e975f59e0c2f8a3eff0ad1e9326f2ca
schliermelvin <miliciiop.77>
2017-11-28 11:44:12 +0100

    update navbar

--- Web/application/views/Nav/navbar.php
+++ Web/application/views/Nav/navbar.php
@@ -12,8 +12,8 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent" >
 
         <ul class="navbar-nav col-lg-3">
-            <li class="nav-item active">
-                <a class="nav-link" href="application/views/Accueil/index.php">Accueil <span class="sr-only">(current)</span></a>
+            <li class="nav-item">
+                <a class="nav-link" href="application/views/Accueil/index.php">Accueil </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="application/views/Forum/index.php">Forum</a>
