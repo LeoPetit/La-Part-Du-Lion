0f8c375277723ce4744e5cc8e56f3507fd85f3c4
JeremyRenaud <j.renaud@student.isartdigital.com>
2017-11-29 09:36:40 +0100

    model utilisateur

---
+++ Web/application/models/Utilisateur_Model.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: jérémy renaud
+ * Date: 28/11/2017
+ * Time: 10:51
+ */
+class Utilisateur_Model
+{
+
+}
---
+++ Web/application/views/Connection/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jérémy renaud
+ * Date: 29/11/2017
+ * Time: 09:30
+ */
