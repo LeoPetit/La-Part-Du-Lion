5693b69ec724700f6761cc6621d4207987b9bf3b
JeremyRenaud <j.renaud@student.isartdigital.com>
2017-11-29 11:23:08 +0100

    formulaire inscription + loadview

--- Web/application/views/Compte/index.php
+++ Web/application/views/Compte/index.php
@@ -5,3 +5,34 @@
  * Date: 24/10/2017
  * Time: 16:21
  */
+?>
+<!DOCTYPE html>
+
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Test de la map</title>
+    <?php $this->load->view('Nav/header.php') ?>
+    <?php $this->load->view('Nav/navbar.php') ?>
+
+
+</head>
+
+
+<body>
+
+
+<div id="bloc_Compte" >
+
+</div>
+
+
+
+</body>
+
+<footer>
+    <?php $this->load->view('Nav/footer.php') ?>
+</footer>
+
+</html>
--- Web/application/views/Connection/index.php
+++ Web/application/views/Connection/index.php
@@ -13,8 +13,8 @@
 <head>
     <meta charset="utf-8">
     <title>Test de la map</title>
-    <?php include("/../Nav/header.php") ?>
-<?php include("/../Nav/navbar.php") ?>
+    <?php $this->load->view('Nav/header.php') ?>
+<?php $this->load->view('Nav/navbar.php') ?>
 
 
 </head>
@@ -47,7 +47,50 @@
             <button class="btn btn-large btn-primary" type="submit">Valider</button>
 
         </form>
+    </div>
+</div>
 
+<div id="bloc_Inscription" >
+
+    <div id="login">
+        <form class ="form_user form-horizontal" method="post" action="">
+            <h4>Inscription</h4>
+
+            <div class="control-group">
+                <label class="control-label" for="Pseudo">Pseudo</label>
+                <div class="controls">
+                    <input class="validate[required]" type="text" name="id" placeholder="Pseudo" value="">
+
+                </div>
+            </div>
+
+            <div class="control-group">
+                <label class="control-label" for="email">Email</label>
+                <div class="controls">
+                    <input class="validate[required,custom[email]]" type="text" name="id" placeholder="Email" value="">
+
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label" for="ConfirmEmail">ConfirmEmail</label>
+                <div class="controls">
+                    <input class="validate[required,custom[email]]" type="text" name="ConfirmEmail" placeholder="ConfirmEmail" value="">
+
+                </div>
+            </div>
+
+            <div class="control-group">
+                <label class="control-label" for="mdp">Mot de passe</label>
+                <div class="controls">
+                    <input class="validate[required]" type="password" id="inputPassword" placeholder="Password" name="mdp">
+
+                </div>
+            </div>
+
+            <button class="btn btn-large btn-primary" type="submit">Valider</button>
+
+        </form>
+    </div>
 </div>
 
 
@@ -55,7 +98,7 @@
 </body>
 
 <footer>
-    <?php include("/../Nav/footer.php") ?>
+    <?php $this->load->view('Nav/footer.php') ?>
 </footer>
 
 </html>
