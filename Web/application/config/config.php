328258dd71dd90ec4deb6cefb8c0d6b8d96bfaf3
schliermelvin <miliciiop.77>
2017-11-30 15:06:59 +0100

    update form

--- Web/application/config/config.php
+++ Web/application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/projet_licence/La-Part-Du-Lion/Web/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
