eaf92f738824de7720e5c378e78dcbaea3c86a93
leo <leo.petit90@gmail.com>
2017-12-28 16:57:09 +0100

    modif redirect connexion

--- Web/application/controllers/Utilisateur_Controller.php
+++ Web/application/controllers/Utilisateur_Controller.php
@@ -45,7 +45,7 @@ class Utilisateur_Controller extends CI_Controller
                 $this->load->view('Connection/index.php', $data);
             } else {
                 $this->session->utilisateur = $isRegistered[0];
-                $this->load->view('Accueil/index.php');
+                $this->load->view('Jeu/map.php');
             }
         }
     }
